517fb1d5a5fdb8a798af9847de1267489e06bb6f
vasiliym <VLMaljutin@stud.kpfu.ru>
2022-04-15 00:20:58 +0300

    Nav-Menu

--- DeliveryCrab/ClientApp/src/app/app.module.ts
+++ DeliveryCrab/ClientApp/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
 
@@ -15,6 +18,8 @@ import { HomeComponent } from './home/home.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
+    LoginComponent,
+    RegisterComponent,
 
   ],
   imports: [
@@ -22,8 +27,11 @@ import { HomeComponent } from './home/home.component';
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      {path: '', component: HomeComponent, pathMatch: 'full'}
-    ])
+      {path: '', component: HomeComponent, pathMatch: 'full'},
+      {path:'login', component: LoginComponent},
+      {path:'register', component: RegisterComponent}
+    ]),
+
 
   ],
   providers: [],
--- DeliveryCrab/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ DeliveryCrab/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
-export class NavMenuComponent implements OnInit {
+export class NavMenuComponent  {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+
 
 }
