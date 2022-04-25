a36f51ce8b0371cf71b092f9c536eb2a6795c403
i.tsyuryupa <openingthedoor@yandex.ru>
2022-04-25 09:49:42 +0300

    Набросок модели ресторанов

---
+++ DeliveryCrab/Models/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿namespace DeliveryCrab.Models
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Icon { get; set; }
+        public User Owner { get; set; }
+
+        public Restaurant(User owner)
+        {
+            this.Owner = owner;
+        }
+    }
+}
