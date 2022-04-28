ebf711ad0f412d11636d30688dd14b506c5e6011
i.tsyuryupa <openingthedoor@yandex.ru>
2022-04-28 17:51:55 +0300

    Изменение контекста

---
+++ DeliveryCrab/Models/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace DeliveryCrab.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<Restaurant> Restaurants { get; set; } = null!;
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
+        
+    }
+}
