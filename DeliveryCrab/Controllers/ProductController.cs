1a90aa71e3257ac564cffd158042c73a8c2b938c
VovaLeder <bliznukvova2@gmail.com>
2022-05-01 14:42:48 +0300

    Еще контроллеры по подобию Юзера

---
+++ DeliveryCrab/Controllers/ProductController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DeliveryCrab.Models;
+namespace DeliveryCrab.Controllers
+{
+
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public ProductController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _context.Products.ToList();
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public Product Get(int id)
+        {
+            Product? product = _context.Products.FirstOrDefault(x => x.Id == id);
+            return product;
+        }
+
+        [HttpPost]
+        [Route("PostProduct")]
+        public IActionResult Post(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Add(product);
+                _context.SaveChanges();
+                return Ok(product);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpDelete]
+        [Route("DeleteProduct")]
+        public IActionResult Delete(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                Product product = _context.Products.First(x => x.Id == id);
+                if (product != null)
+                {
+                    _context.Products.Remove(product);
+                    _context.SaveChanges();
+                    return Ok(id);
+                }
+                return BadRequest(); // Честно хз, что отправлять, если пытаться удалить юзера, id которого нет, так что пока так
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPut]
+        [Route("PutProduct")]
+        public IActionResult Put(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                Product old_product = _context.Products.First(x => x.Id == product.Id);
+                old_product.Id = product.Id;
+                old_product.Name = product.Name;
+                old_product.Icon = product.Icon;
+                old_product.Price = product.Price;
+                old_product.Description = product.Description;
+                old_product.RestaurantId = product.RestaurantId;
+
+                _context.SaveChanges();
+                return Ok(product);
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ DeliveryCrab/Controllers/RestaurantController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DeliveryCrab.Models;
+namespace DeliveryCrab.Controllers
+{
+
+    [Route("[controller]")]
+    [ApiController]
+    public class RestaurantController : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public RestaurantController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Restaurant> Get()
+        {
+            return _context.Restaurants.ToList();
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public Restaurant Get(int id)
+        {
+            Restaurant? restaurant = _context.Restaurants.FirstOrDefault(x => x.Id == id);
+            return restaurant;
+        }
+
+        [HttpPost]
+        [Route("PostRestaurant")]
+        public IActionResult Post(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Restaurants.Add(restaurant);
+                _context.SaveChanges();
+                return Ok(restaurant);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpDelete]
+        [Route("DeleteRestaurant")]
+        public IActionResult Delete(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                Restaurant restaurant = _context.Restaurants.First(x => x.Id == id);
+                if (restaurant != null)
+                {
+                    _context.Restaurants.Remove(restaurant);
+                    _context.SaveChanges();
+                    return Ok(id);
+                }
+                return BadRequest(); // Честно хз, что отправлять, если пытаться удалить юзера, id которого нет, так что пока так
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPut]
+        [Route("PutRestaurant")]
+        public IActionResult Put(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                Restaurant old_restaurant = _context.Restaurants.First(x => x.Id == restaurant.Id);
+                old_restaurant.Id = restaurant.Id;
+                old_restaurant.Name = restaurant.Name;
+                old_restaurant.Icon = restaurant.Icon;
+                old_restaurant.Address = restaurant.Address;
+                old_restaurant.Userid = restaurant.Userid;
+
+                _context.SaveChanges();
+                return Ok(restaurant);
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
